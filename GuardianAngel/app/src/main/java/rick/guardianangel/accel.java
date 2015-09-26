5e36c62216b9eb228ffc5df8ff90e753e27ad178
rkyuan <hxy153@case.edu>
2015-09-26 17:27:00 -0400

    working on accel class

--- GuardianAngel/app/src/main/java/rick/guardianangel/accel.java
+++ GuardianAngel/app/src/main/java/rick/guardianangel/accel.java
@@ -9,18 +9,48 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.widget.TextView;
+import java.lang.Math;
 
 /**
  * Created by Rick on 9/26/2015.
  */
 public class accel extends Activity implements SensorEventListener{
+    public final int SAMPLERATE_US = 10;
+
+    private final SensorManager mSensorManager;
+    private final Sensor mAccelerometer;
+    private double[] accelData;
+    private int counter;
+
+    public accel() {
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accelData = new double[5000000/SAMPLERATE_US];
+
+    }
 
 
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    sensorManager = (sensorManager) getSystemService(Context.SENSOR_SERVICE);
-    accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SAMPLERATE_US);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        double magnitude= Math.sqrt(Math.pow(event.values[0],2)+
+                                    Math.pow(event.values[1],2)+
+                                    Math.pow(event.values[2],2))-9.8;
+
+
+    }
 
 
 
